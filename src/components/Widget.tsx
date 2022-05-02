64972c2ecf82ba5af132064a7b5f5f42437a2d0e
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2022-05-02 20:38:31 -0300

    Aula 1

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,7 +1,20 @@
-export function Widget(){
+import { Popover } from "@headlessui/react";
+import {ChatTeardropDots} from "phosphor-react";
+
+export function Widget() {
     return (
-    <div className="widget">
-      <h1>Widget</h1>
-    </div>
-  );
+        <Popover className={'absolute bottom-5 right-5'}>
+
+            <Popover.Panel>
+                <p>This should be a panel</p>
+            </Popover.Panel>
+
+            <Popover.Button className={'group bg-br rounded-full px-3 h-12 text-white bg-brand-500 flex items-center'}>
+                <ChatTeardropDots className={'w-6 h-6'}/>
+                <span className={'max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'}>
+                    <span className={'pl-2'} >Feedback</span>
+                </span>
+            </Popover.Button>
+        </Popover>
+    );
 }
